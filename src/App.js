641641ef5ac1ec298100ce56eeb1508bcc1c7e9c
Ahmet Altoprak <118135125+ahmetaltoprakx@users.noreply.github.com>
2024-09-23 20:23:29 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import "./styles.css";
+
+import Spline from "@splinetool/react-spline";
+
+export default function App() {
+  return (
+    <Spline scene="https://prod.spline.design/g5ckTtWF3f8EW9vL/scene.splinecode" />
+  );
+}
