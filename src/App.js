c1b15a684060683d9e7554008c93de28cd97cf5c
Ahmet Altoprak <118135125+ahmetaltoprakx@users.noreply.github.com>
2024-09-23 17:37:20 +0000

    yeni navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import "./styles.css";
-
 import Spline from "@splinetool/react-spline";
 
 export default function App() {
   return (
-    <Spline scene="https://prod.spline.design/g5ckTtWF3f8EW9vL/scene.splinecode" />
+    <div className="app-container">
+      <Spline scene="https://prod.spline.design/g5ckTtWF3f8EW9vL/scene.splinecode" />
+
+      {/* Navbar */}
+      <nav className="navbar">
+        <ul>
+          <li>
+            <a href="#about">Hakkında</a>
+          </li>
+          <li>
+            <a href="#products">Ürünler</a>
+          </li>
+          <li>
+            <a href="#contact">İletişim</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 }
